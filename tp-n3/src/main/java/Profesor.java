d4923578c7f356f0c7c30789f8b1a831f9ada324
RenzoZav <102383387+RenzoZav@users.noreply.github.com>
2023-10-07 02:27:30 -0300

    Delete tp-n3 directory

--- tp-n3/src/main/java/Profesor.java
+++
@@ -1,9 +0,0 @@
-
-public class Profesor extends Persona{
-    private String rol = "Profesor";
-    public Profesor(String nombre, int edad, String rol){
-        super(nombre, edad);
-        this. rol = rol;
-    }
-    
-}
